32159405e45e272a7c54a3c9a75bd304db6efe30
princekr96 <princekr700@gmail.com>
2021-07-04 14:40:19 +0530

    improved styling and fixed hour issue

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 const hourHand = document.querySelector('.hand-hr');
 const minuteHand = document.querySelector('.hand-min');
 const secondHand = document.querySelector('.hand-sec');
+const date = document.querySelector('.date');
 
 function getTime() {
   const now = new Date();
 
-  const hours = now.getHours();
-  const hoursDegree = (hours / 60 / 12) * 360 + 90;
-  hourHand.style.transform = `rotate(${hoursDegree}deg)`;
+  const seconds = now.getSeconds();
+  const secondsDegree = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegree}deg)`;
 
   const minutes = now.getMinutes();
   const minutesDegree = (minutes / 60) * 360 + 90;
   minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
 
-  const seconds = now.getSeconds();
-  const secondsDegree = (seconds / 60) * 360 + 90;
-  secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+  const hours = now.getHours();
+  const hoursDegree = (hours / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegree}deg)`;
 }
 
 setInterval(getTime, 1000);
